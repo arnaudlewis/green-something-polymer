27d44fc6161b0c6909268c82f15c9967b57dd279
Olivier Couliibaly <olivier.ingesup@gmail.com>
2016-03-08 23:45:14 +0100

    refactoring

--- utils/utils.js
+++ utils/utils.js
@@ -3,11 +3,8 @@ var config = require('./jwtConfig');
 
 var middlewares = {
     verifyToken : function(req, res, next) {
-    // check header or url parameters or post parameters for token
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    // decode token
     if (token) {
-      // verifies secret and checks exp
       jwt.verify(token, config.secret, function(err, decoded) {
         if (err) {
           return res.status(403).send('Failed to authenticate token.');
